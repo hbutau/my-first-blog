89049b11f05f0249a3672e48ec1030c4ff9b4346
Anna Makarudze <amakarudze@gmail.com>
2016-03-20 01:33:26 +0200

    Changed database

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,8 +82,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'annablog',
+        'USER': 'amakarudze',
+        'PASSWORD': 'T@wana03',
+        'OPTIONS': {
+          'autocommit': True,
+        },
+        'HOST': '',
+        'PORT': '',
     }
 }
 
