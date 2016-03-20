bd04d547dfca62133d9b2e79b544a48d79d4565a
Anna Makarudze <amakarudze@gmail.com>
2016-03-21 01:48:29 +0200

    registered article model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Contact, Comment, Event
+from .models import Post, Contact, Comment, Event, Article
 
-admin.site.register(Post)
+admin.site.register(Article)
 admin.site.register(Contact)
 admin.site.register(Comment)
 admin.site.register(Event)
+admin.site.register(Post)
