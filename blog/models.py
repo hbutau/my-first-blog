223cc6526446b2698033baa358c0a95f58dc1e6c
Anna Makarudze <amakarudze@gmail.com>
2016-03-20 22:48:32 +0200

    Slug unique

--- blog/models.py
+++ blog/models.py
@@ -11,7 +11,7 @@ class Post(models.Model):
     default=timezone.now)
     published_date = models.DateTimeField(
     blank=True, null=True)
-    slug = AutoSlugField(populate_from='title')
+    slug = AutoSlugField(populate_from='title', unique=True)
 
     def publish(self):
         self.published_date = timezone.now()
