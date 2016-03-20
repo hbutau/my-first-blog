01c6533fab06d8722f4c338112d07e1a4221e164
Anna Makarudze <amakarudze@gmail.com>
2016-03-20 22:09:24 +0200

    Revert to Sqlite database

--- mysite/settings.py
+++ mysite/settings.py
@@ -82,15 +82,8 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'amakarudze$default',
-        'USER': 'amakarudze',
-        'PASSWORD': 'T@wana03',
-        'OPTIONS': {
-          'autocommit': True,
-        },
-        'HOST': '',
-        'PORT': '',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
